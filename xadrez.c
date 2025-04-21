441df5f729859d301cf161f802f997146b5559fc
mateusdemirandapereira <terrymafa@gmail.com>
2025-04-21 12:40:38 -0300

    implementado o movendo do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -31,5 +31,41 @@ int main() {
 	} while (j < 8);
 
 
+
+
+	// implementado o movimento do cavalor 
+	printf("\n");
+	
+	printf("Movendo o Cavalo em L\n");
+
+	int numeroCasasBaixo = 2;
+
+	int numeroCasasEsquerda = 1;
+
+	for (int i = 1; i<=1; i++) {
+
+		// implementado o movimento para baixo
+		int j = 1;
+		while(j <= numeroCasasBaixo) {
+
+			printf("%s\n", "baixo");
+
+			j = j +1;
+		}
+
+		// implementado o movimento para a esquerda
+		int y = 1;
+
+		do {
+
+			printf("%s\n", "esquerda");
+
+			y = y + 1;
+		} while (y <= numeroCasasEsquerda);
+
+
+
+	}
+
 	return 0;
 }
